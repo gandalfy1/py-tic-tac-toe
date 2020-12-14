6d94a7710453c88ffbc1c5525cfd31a36b781e27
frodobaggins <frodobaggins@frodobagginss-MacBook-Pro.local>
2020-12-14 01:55:36 -0800

    adds helpful comments

--- run_game.py
+++ run_game.py
@@ -1,17 +1,22 @@
  
 #prints the board game given current board state
 def printBoard(boardState):
-    print(boardState["11"]+" " + boardState["12"]+ " "+boardState["13"])
-    print(boardState["21"]+" " + boardState["22"]+ " "+boardState["23"])
-    print(boardState["31"]+" " + boardState["32"]+ " "+boardState["33"])
+    print(boardState["11"]+" " + boardState["12"]+ " "+boardState["13"]) # row 1
+    print(boardState["21"]+" " + boardState["22"]+ " "+boardState["23"]) # row 2
+    print(boardState["31"]+" " + boardState["32"]+ " "+boardState["33"]) # row 3
     return
 
 
-#updates board given use input
-def handlePlayerMove(boardState, position, player):
+#updates board given user input
+#`position` is the user input
+def handlePlayerMove(boardState, position, player): 
     isValid = False
+
+    #if user enter a valid position on the board
     if position in boardState:
+        #if the user selected an open/valid position on the board
         if boardState[position] not in ["X","O"]:
+            # update board
             boardState[position] = "X" if player == 1 else "O"
             isValid = True
 
@@ -35,17 +40,29 @@ positionCache = {
 #start game execution code
 print("type 'exit' to end \n\n")
 
-player = 1
+#keeps track of player turn 
+#  1 for player 1
+# -1 for player 2
+playerTurn = 1
+
+# infinit loop keeps game
 while True:
     printBoard(positionCache)
     print("\n\n")
-    playerName = "1" if player == 1 else "2"
+    
+    # prompts user for input
+    # build next player's turn prompt
+    playerName = "1" if playerTurn == 1 else "2"
     move = raw_input("Player" + playerName + "'s move: ") 
+
+    # parse user inputs
     if move == "exit":
         break
-    isValidMove = handlePlayerMove(positionCache, move, player)
+    isValidMove = handlePlayerMove(positionCache, move, playerTurn)
+
+    # validate input , if valid then move to next players turn otherwise 
     if isValidMove:
-        player *= -1
+        playerTurn *= -1
 
 
 
